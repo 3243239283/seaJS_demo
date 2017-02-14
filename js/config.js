710c437bfecebde4489aebb6a0eb11c383b330f3
3243239283 <treeclass@qq.com>
2017-02-14 11:53:31 +0800

    学习

--- js/config.js
+++ js/config.js
@@ -3,14 +3,15 @@ seajs.config({
 	base:'../',
 	alias: {
 		'jquery': 'js/moudle/jquery/jquery',
-		'all': 'js/all',
-		'hand':'js/hand',
 		'handlebars':'js/moudle/handlebars/handlebars',
 		'layer':'js/moudle/layer/layer',
 		'laypage':'js/moudle/page/laypage/laypage',
 		'laytpl':'js/moudle/laytpl/laytpl',
 		'laycss':'js/moudle/layer/skin/layer.css',
 		'laypagecss':'js/moudle/page/laypage/skin/laypage.css',
-		'stylecss': '../css/index.css'
+		'stylecss': '../css/index.css',
+		'all': 'js/all',
+		'hand':'js/hand',
+		'five':'js/five'
 	}
 });
